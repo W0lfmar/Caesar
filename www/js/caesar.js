ec110697fb4abcb600c2f6bf175e51ef75f2b711
juangualberto <jgutierrez@iesvirgendelcarmen.com>
2017-03-17 10:43:04 +0100

    Primera carga del proyecto

---
+++ www/js/caesar.js
@@ -0,0 +1,26 @@
+/*jslint browser:true, devel:true, white:true, vars:true */
+/*global $:false, intel:false */
+// variables para el jslint
+
+
+function caesar(texto, clave) {
+    var output="";
+    clave = parseInt(clave);
+    for (var i=0; i<texto.length;i++){
+		output += String.fromCharCode(texto.charCodeAt(i)+clave);
+    }
+    return output;    
+}
+
+function cifrar(){
+    var texto = $("#texto_cifrar").val();
+    var clave = $("#clave_cifrar").val();
+    $("#texto_desc").val(caesar(texto,clave));
+}
+
+function descifrar(){
+    var texto = $("#texto_desc").val();
+    var clave = $("#clave_desc").val();
+    $("#texto_cifrar").val(caesar(texto,-clave));
+}
+
