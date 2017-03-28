055749b11b277ff41869834a75028091fbb82b1c
Juan Gualberto <juangualberto@users.noreply.github.com>
2017-03-28 10:19:28 +0200

    Update caesar.js

--- www/js/caesar.js
+++ www/js/caesar.js
@@ -15,12 +15,12 @@ function caesar(texto, clave) {
 function cifrar(){
     var texto = $("#texto_cifrar").val();
     var clave = $("#clave_cifrar").val();
-    $("#texto_desc").val(btoa(caesar(texto,clave)));
+    $("#texto_desc").val(caesar(texto,clave));
 }
 
 function descifrar(){
     var texto = $("#texto_desc").val();
     var clave = $("#clave_desc").val();
-    $("#texto_cifrar").val(atob(caesar(texto,-clave)));
+    $("#texto_cifrar").val(caesar(texto,-clave));
 }
 
